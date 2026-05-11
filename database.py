996023528072ba766bd3a65bb39a2a2987de6696
Yashgiradkar-210401 <yashlgiradkar210401@gmail.com>
2026-05-11 21:11:22 +0530

    Final NexusIQ Cloud Deployment

--- database.py
+++ database.py
@@ -1,5 +1,7 @@
 from sqlalchemy import create_engine
 
+DATABASE_URL = "postgresql+psycopg2://neondb_owner:npg_zkZl6OmfIYL9@ep-winter-lake-aqh78g6b-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+
 engine = create_engine(
-    "postgresql+psycopg2://postgres:root@localhost:5432/nexusiq"
+    DATABASE_URL
 )
